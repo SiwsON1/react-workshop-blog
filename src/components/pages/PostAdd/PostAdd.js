c29275b6bff46804253171fb8bd831737f674ba3
Marcin Siwonia <mahinek12@gmail.com>
2023-03-22 19:18:18 +0100

    Add PostForm

--- src/components/pages/PostAdd/PostAdd.js
+++ src/components/pages/PostAdd/PostAdd.js
@@ -1,8 +1,13 @@
+import AddPostForm from "../../features/AddPostForm/AddPostForm";
+import styles from './PostAdd.module.scss';
+
 const PostAdd = () => {
   return (
-    
-     <h1>PostAdd</h1>
-      
+    <section className ={styles.postForm}>
+    <h1>Add Post</h1>
+     <AddPostForm />
+    </section>
+     
     
   );
 };
